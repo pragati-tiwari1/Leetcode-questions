e1089d82fca7e91a859be03d587e8216411c6ebb
Pragati Tiwari <133578513+pragati-tiwari1@users.noreply.github.com>
2024-07-29 15:07:07 +0530

    Time: 15 ms (88.75%), Space: 42.9 MB (23.49%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.java
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -2,17 +2,29 @@ class Solution {
     public int numTeams(int[] rating) {
         int c=0;
         for(int i =0;i<rating.length;i++){
-            for(int j =i+1;j<rating.length;j++){
-                for(int k=j+1;k<rating.length;k++){
-                    if(rating[i]<rating[j] && rating[j]<rating[k]){
-                        c++;
-                    }
-                    else if(rating[i]>rating[j] && rating[j]>rating[k]){
-                        c++;
-                    }
-                }
-            }
+            c+=count(rating[i],rating.length,rating,i);
         }
         return c;
+    }
+    public int count(int curr,int n,int[] rating,int idx){
+        int left[] =countGreaterOrSmaller(rating,0,idx,curr);
+        int right[] =countGreaterOrSmaller(rating,idx+1,n,curr);
+
+        int teams = 0;
+        teams += left[0]*right[1];//ascending
+        teams += left[1]*right[0];//descending
+        return teams;
+    }
+    public int[] countGreaterOrSmaller(int rating[],int s,int e,int curr){
+       int small=0;
+       int large=0;
+        for(int i =s;i<e;i++){
+            if(rating[i]<curr){
+                small++;
+            }else{
+                large++;
+            }
+        }
+        return new int[]{small,large};
     }
 }
