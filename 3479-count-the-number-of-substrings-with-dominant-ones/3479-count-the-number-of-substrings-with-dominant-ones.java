fc08025d0bf2c4ef0ab083416abdbbecfdce63b5
Pragati Tiwari <133578513+pragati-tiwari1@users.noreply.github.com>
2024-08-08 17:45:49 +0530

    Time: 1377 ms (38.67%), Space: 45.2 MB (64.57%) - LeetHub

---
+++ 3479-count-the-number-of-substrings-with-dominant-ones/3479-count-the-number-of-substrings-with-dominant-ones.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int n = s.length();
+        int pre[] = new int[n];
+        pre[0] = (((int)(s.charAt(0) - '0')) == 1 ? 1 : 0);
+        for(int i =1;i<n;i++){
+            pre[i] = pre[i-1]+ (((int)(s.charAt(i) - '0')) == 1 ? 1 : 0);
+        }
+        int ans=0;
+        for(int i =0;i<n;i++){
+            int one =0;
+            int zero=0;
+            for(int j =i;j<n;j++){
+                one = pre[j] - (i==0?0:pre[i-1]);
+                zero = (j-i+1)-one;
+                if(zero*zero>one){
+                    j+=(zero*zero-one-1);
+                }
+                else if(zero*zero == one){
+                    ans++;
+                } 
+                else if(zero*zero<one){
+                    ans++;
+                    int diff = (int)Math.sqrt(one)-zero;
+                    int nextj = j+diff;
+                    if(nextj >=n ){
+                        ans+=(n-j-1);
+                    }else{
+                        ans+=diff;
+                    }
+                    j = nextj;
+                }
+            }
+        }
+        return ans;
+    }
+}
